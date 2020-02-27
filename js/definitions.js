f3074a3f504275d43892a08dd60568dc6fb3632e
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-02-26 23:17:46 -0800

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,95 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "Aquarium PCR Models",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/dvnstrcklnd/aq-pcr-models/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'CommonInputOutputNames',
+              path: 'libraries/CommonInputOutputNames' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'MasterMixHelper',
+              path: 'libraries/MasterMixHelper' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'PCRComposition',
+              path: 'libraries/PCRComposition' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'PCRProgram',
+              path: 'libraries/PCRProgram' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Units',
+              path: 'libraries/Units' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+        ]
+      }
+    
+
+  ]
+
+};
