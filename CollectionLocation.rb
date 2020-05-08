0dde16a38626682f8701b3f24d45f1a0896c2c93
Amy Cash <amycash@uw.edu>
2020-05-08 12:27:56 -0700

    Change variable names, clarify Yard descriptions

--- CollectionLocation.rb
+++ CollectionLocation.rb
@@ -3,7 +3,7 @@
 # Cannon Mallory
 # malloc3@uw.edu
 #
-# This is to facilitate sample management within collection
+# Methods to facilitate sample management within collections
 module CollectionLocation
 
   ALPHA26 = ('A'...'Z').to_a
@@ -12,23 +12,22 @@ module CollectionLocation
   #
   # @param collection [Collection] the collection containing the sample
   # @param sample [Sample] the Sample that you want to locate
-  # @return [String] the Alpha Numerica location(s) e.g. A1, A2
+  # @return [String] the Alpha Numeric location(s) e.g. A1, A2
   def get_alpha_num_location(collection, sample)
-    loc_array = get_item_sample_location(collection, sample)
-    # [[r0, c0],[r1, c0], [r2,c0]]
-    location = []
-    loc_array.each do |loc| # takes coords [2, 0] index=0
-      location << convert_rc_to_alpha(loc) # 2,0 -> C1, 4,0 -> E1
+    coordinates = get_item_sample_location(collection, sample) # [[r0, c0],[r1, c0], [r2,c0]]
+    alpha_num_locations = []
+    coordinates.each do |coordinate_set| # takes coords [2, 0] index=0
+      alpha_num_locations << convert_rc_to_alpha(coordinate_set) # 2,0 -> C1, 4,0 -> E1
     end
-    location.join(',') # removes the ["A1"] the brackets and parantheses
+    locations.join(',') # removes the ["A1"] the brackets and parantheses
   end
 
-  # converts an array of R,C to alpha numerical location
+  # converts an array containing one set of row and column values coordinates to alpha numerical locations
   #
-  # @param loc [Array<r,c>] array of row and column
+  # @param coordinates [Array<row,column>] set of coordinates
   # @return [String] alpha numerical location
-  def convert_rc_to_alpha(loc)
-    ALPHA26[loc[0]] + (loc[1] + 1).to_s
+  def convert_rc_to_alpha(coordinates)
+    ALPHA26[coordinates[0]] + (coordinates[1] + 1).to_s
   end
 
   # Converts alpha numerical location to Array<r,c>
@@ -42,10 +41,10 @@ module CollectionLocation
     [row, col]
   end
 
-  # Finds the location of an item or sample
+  # Finds the location coordinates of an Item or Sample
   #
-  # @param collection [Collection] the collection containing the item or sample
-  # @param part [Item, Part, Sample] item, part, or sample to be found
+  # @param collection [Collection] the Collection containing the Item or Sample
+  # @param part [Item, Part, Sample] Item, Part, or Sample to be found
   # @return [Array] Array of item, part, or sample locations in form [[r1,c1],[r2,c1]]
   def get_item_sample_location(collection, part)
     collection.find(part)
@@ -54,13 +53,12 @@ module CollectionLocation
   # Finds a sample from an alpha numberical string location(e.g. A1, B1)
   #
   # @param collection [Collection] the collection that contains the part
-  # @param loc [String] the location of the part within the collection (A1, B3, C7)
+  # @param  alpha [String] the location of the part within the collection (A1, B3, C7)
   # @return part [Item] the item at the given location
-  def part_alpha_num(collection, loc)
-    row, col = convert_alpha_to_rc(loc)
-    dem = collection.dimensions
-    raise 'Location outside collection dimensions' if row > dem[0] || col > dem[1]
-
+  def part_alpha_num(collection, alpha)
+    row, col = convert_alpha_to_rc(alpha)
+    dimensions = collection.dimensions
+    raise 'Location outside collection dimensions' if row > dimensions[0] || col > dimensions[1]
     part = collection.part(row, col)
   end
 
@@ -78,10 +76,10 @@ module CollectionLocation
   def get_rcx_list(collection, samples)
     rcx_list = []
     array_of_samples.each do |sample|
-      sample_rc = get_item_sample_location(from_collection, sample)
+      sample_coordinates = get_item_sample_location(from_collection, sample)
       sample_alpha = get_alpha_num_location(from_collection, sample)
 
-      sample_rc.each do |coordinates|
+      sample_coordinates.each do |coordinates|
         coordinates.push(sample_alpha) # [0,0,A1]
         locations.push(coordinates)
       end
@@ -89,4 +87,3 @@ module CollectionLocation
     rcx_list
   end
 end
-
