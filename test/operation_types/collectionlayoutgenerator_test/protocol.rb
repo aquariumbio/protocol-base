e40a27b736bd788b38bc764f7115aae47d7fa27b
Devin Strickland <strcklnd@uw.edu>
2020-06-08 09:53:22 -0700

    added CollectionLayoutGenerator

---
+++ test/operation_types/collectionlayoutgenerator_test/protocol.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+needs 'Standard Libs/Debug'
+needs 'Collection Management/CollectionLayoutGenerator'
+
+# Protocol for setting up a plate with extracted RNA samples
+#
+# @author Devin Strickland <strcklnd@uw.edu>
+class Protocol
+  include Debug
+
+  def main
+    raise 'Too many' if operations.length > 22
+
+    layout_table = Array.new(8) { |_| Array.new(12) }
+
+    clg = CollectionLayoutGeneratorFactory.build(group_size: 3)
+
+    clg.next_group.each { |r, c| layout_table[r][c] = 'NTC' }
+
+    clg.next_group(column: 11).each { |r, c| layout_table[r][c] = 'nCoVPC' }
+
+    operations.each do |op|
+      clg.next_group.each { |r, c| layout_table[r][c] = op.id }
+    end
+
+    show do
+      table layout_table
+    end
+
+    {}
+  end
+end
