1bfb3b68ecc07dd15178d0bdc615226aeaad0aa4
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-06-08 14:24:38 -0700

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,77 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "Aquarium Microtiter Plates",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/dvnstrcklnd/aq-microtiter-plates/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'MicrotiterPlate',
+              path: 'libraries/MicrotiterPlate' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'PlateLayoutGenerator',
+              path: 'libraries/PlateLayoutGenerator' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+        ]
+      }
+    
+
+  ]
+
+};
