8894e8ce644cd50700ee8ae324fdbd4c3d41c353
Devin Strickland <strcklnd@uw.edu>
2020-12-24 21:52:53 -0500

    restored pipettors test

--- test/operation_types/pipettors_test/test.rb
+++ test/operation_types/pipettors_test/test.rb
@@ -1,14 +1,11 @@
+# frozen_string_literal: true
+
 class ProtocolTest < ProtocolTestBase
-
   def setup
-
-      add_random_operations(3)
-
+    add_random_operations(1)
   end
 
   def analyze
-      log('Hello from Nemo')
-      assert_equal(@backtrace.last[:operation], 'complete')
+    assert_equal(@backtrace.last[:operation], 'complete')
   end
-
 end
