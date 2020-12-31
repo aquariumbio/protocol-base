41fc2581b6d35d58a6e0bf9b3d3d7710f814bf0c
Cannon Mallory <malloc3@uw.edu>
2020-12-31 11:06:04 -0800

    Addition of consumables first push

---
+++ consumablelibs/libraries/consumabledefinitions/source.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+needs 'Standard Libs/Units'
+
+module ConsumableDefinitions
+
+  include Units
+
+  PLATE_96_WELL = '96 Well Plate'
+  PLATE_384_WELL = '384 Well Plate'
+  AREA_SEAL = "Microseal 'B' adhesive seals"
+  MICRO_TUBE = '1.7 ml Tube'
+  SPARE_PLATE = '96 Well Plate'
+  TEST_TUBE = '15 ml Reagent Tube'
+  TUBE_500UL = '0.5 ml Tube'
+  QBIT_TUBE = 'Qubit Assay Tube'
+
+  TIP_BOX_100 = "100 #{MICROLITERS} Pipette Tip Box"
+
+  TYPES = {
+    plate: 'Plate',  # A multi well plate (24, 96, 384)
+    petri_dish: 'Petri Dish',  # Sometimes known as a Plate as well
+    bottle: 'Bottle',  # A standard bottle
+    reagent_bottle: 'Reagent Bottle',  # A bottle that holds common reagents
+    cover: 'Cover',  # Aluminum Foil, Cling Wrap
+    pipette_tips: 'Pipette Tips' # obvious
+  }.freeze
+
+  LOCATIONS = {
+    location_1: 'That one spot over by the fridge'
+  }
+
+  # A 'consumable' is an item that is not part of a reaction but is required
+  #   for the procedure to be performed.
+
+  # A 'Type' is a grouping of consumables.  This needs to be expanded a little
+  #    general like 'plates" or 'Pipette tips' or 'containers'
+
+  CONSUMABLES = {
+    PLATE_96_WELL => {
+      name: PLATE_96_WELL,
+      type: TYPES[:plate],
+      location: LOCATIONS[:location_1],
+      description: 'Description'
+    },
+    PLATE_384_WELL => {
+      name: PLATE_384_WELL,
+      type: TYPES[:plate],
+      location: LOCATIONS[:location_1],
+      description: 'Description'
+    },
+    AREA_SEAL => {
+      name: AREA_SEAL,
+      type: TYPES[:bottle],
+      location: LOCATIONS[:location_1],
+      description: 'Description'
+    },
+    MICRO_TUBE => {
+      name: MICRO_TUBE,
+      type: TYPES[:bottle],
+      location: LOCATIONS[:location_1],
+      description: 'Description'
+    },
+    SPARE_PLATE => {
+      name: SPARE_PLATE,
+      type: TYPES[:bottle],
+      location: LOCATIONS[:location_1],
+      description: 'Description'
+    },
+    TEST_TUBE => {
+      name: TEST_TUBE,
+      type: TYPES[:bottle],
+      location: LOCATIONS[:location_1],
+      description: 'Description'
+    },
+    TUBE_500UL => {
+      name: TUBE_500UL,
+      type: TYPES[:bottle],
+      location: LOCATIONS[:location_1],
+      description: 'Description'
+    },
+    QBIT_TUBE => {
+      name: QBIT_TUBE,
+      type: TYPES[:bottle],
+      location: LOCATIONS[:location_1],
+      description: 'Description'
+    },
+    TIP_BOX_100 => {
+      name: TIP_BOX_100,
+      type: TYPES[:pipette_tips],
+      location: LOCATIONS[:location_1],
+      description: TIP_BOX_100
+    }
+  }.freeze
+ 
+end
---
+++ consumablelibs/libraries/consumables/source.rb
@@ -0,0 +1,130 @@
+# frozen_string_literal: true
+
+needs 'Standard Libs/Units'
+
+# Consumables Factory
+
+class ConsumablesFactory
+  def self.build(consumable_data:)
+    Consumables.new(consumable_data: consumable_data)
+  end
+end
+
+# Consumables class
+
+class Consumables
+
+  attr_reader :consumables
+
+  def initialize(consumable_data: nil)
+    @consumables = []
+    add_consumables(consumable_data: consumable_data)
+  end
+
+  def add_consumables(consumable_data:)
+    consumable_data.each do |con|
+      existing_input = find_input(con[:consumable][:name])
+      if existing_input.present?
+        existing_input.add(qty: qty, units: units)
+      else
+        @consumables.append(Consumable.new(con))
+      end
+    end
+  end
+
+  # Retrieves components by input_name for array of strings or single string
+  #
+  # @param input_name [String] or [Array<String>]
+  # @return [Component] or [Array<Component>]
+  def input(input_name)
+    if input_name.is_a? Array
+      input_name.map{ |iname| find_input(iname) }
+    else
+      find_input(input_name)
+    end
+  end
+
+  private
+
+  # Retrieves components by input_name for a single string
+  #
+  # @param input_name [String] or [Array<String>]
+  # @return [Component] or [Array<Component>]
+  def find_input(input_name)
+    consumables.find { |c| c.input_name == input_name }
+  end
+end
+
+# Modules a component needed for a biochemical reaction
+#
+# Super class over both 'reagents'
+class Consumable
+  include Units
+
+  attr_reader :input_name, :qty, :units, :notes
+  attr_accessor :location,
+                :description,
+                :adj_qty,
+
+  LOT_NUM = 'Lot_Number'
+
+  def initialize(consumable:, qty:, units:,  notes: nil)
+    @input_name = consumable[:name]
+    @qty = qty
+    @units = units
+    @location = consumable[:location]
+    @description = consumable[:description]
+    @type = consumable[:type]
+    @adj_qty = qty
+    @notes = notes
+  end
+
+  # To string method
+  #
+  # @return [String]
+  def to_s
+    input_name.to_s
+  end
+
+  # The volume as a qty, units hash
+  #
+  # @param adj_qty [Boolean] true if show adjusted quantity
+  # @return [Hash]
+  def volume_hash(adj_qty: false)
+    { qty: adj_qty ? @adj_qty : @qty, units: units }
+  end
+
+  # Displays the volume (`qty`) with units
+  #
+  # @return [String]
+  def qty_display(round = 1, adj_quantities: false)
+    amount = if adj_quantities
+               adj_qty
+             else
+               qty
+             end
+    amount = amount.round(round) unless amount.nil?
+    Units.qty_display({ qty: amount, units: units })
+  end
+
+  def add(qty:, units:)
+    raise 'units dont match' unless units == @units
+    @qty += qty
+  end
+
+  # Adjusts the qty by a given factor and, if needed, makes it checkable
+  #   in a table
+  #
+  # @param mult [Float] the factor to multiply `qty` by
+  # @param round [FixNum] the number of places to round the result to
+  # @param checkable [Boolean] whether to make the result checkable
+  #   in a table
+  # @return [Numeric, Hash]
+  def adjusted_qty(mult = 1.0, round = 1)
+    return if qty.nil?
+
+    raise "Multiplier is nil for composition #{input_name}" if mult.nil?
+
+    @adj_qty = (qty * mult).round(round)
+  end
+end
